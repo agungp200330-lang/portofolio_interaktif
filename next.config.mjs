81400b8d499f2bd280ab59cb4554adb64f211fcf
agungPrayogi-ap <agungp200330@gmail.com>
2026-03-16 11:19:54 +0700

    Menambahkan GitHub Actions deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Penting untuk GitHub Pages
+  basePath: '/portofolio_interaktif', // Nama repositori GitHub kamu
+  assetPrefix: '/portofolio_interaktif/', // Tambahkan garis miring di akhir
   images: {
     unoptimized: true, // Karena GitHub Pages tidak punya server image optimization
   },
+
 };
 
 export default nextConfig;
