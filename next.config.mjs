cc1d12b35179d30b9d093ed52e89dff6a177e415
agungPrayogi-ap <agungp200330@gmail.com>
2026-03-16 11:16:38 +0700

    konfigurasi untuk github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // Penting untuk GitHub Pages
+  images: {
+    unoptimized: true, // Karena GitHub Pages tidak punya server image optimization
+  },
 };
 
 export default nextConfig;
